322da7e4ba4eac64c91adc398112b6ba5354ce60
nagendrayakkaladevara@gmail.com <nagendrayakkaladevara@gmail.com>
2025-07-13 11:47:45 +0530

    first commit

--- data/busRoutes.ts
+++ data/busRoutes.ts
@@ -8,9 +8,9 @@ export const busRoutes: BusRoute[] = [
     stops: [
       { id: '1', name: 'Central Station', code: 'CS001', audioFile: 'route101_stop1.mp3' },
       { id: '2', name: 'City Hall', code: 'CH002', audioFile: 'route101_stop2.mp3' },
-      { id: '3', name: 'Business District', code: 'BD003', audioFile: 'route101_stop3.mp3' },
-      { id: '4', name: 'Shopping Mall', code: 'SM004', audioFile: 'route101_stop4.mp3' },
-      { id: '5', name: 'University', code: 'UN005', audioFile: 'route101_stop5.mp3' }
+      { id: '3', name: 'Business District', code: 'BD003', audioFile: 'route101_stop1.mp3' },
+      { id: '4', name: 'Shopping Mall', code: 'SM004', audioFile: 'route101_stop2.mp3' },
+      { id: '5', name: 'University', code: 'UN005', audioFile: 'route101_stop1.mp3' }
     ]
   },
   {
@@ -20,8 +20,8 @@ export const busRoutes: BusRoute[] = [
     stops: [
       { id: '1', name: 'Airport Terminal', code: 'AT001', audioFile: 'route205_stop1.mp3' },
       { id: '2', name: 'Hotel Plaza', code: 'HP002', audioFile: 'route205_stop2.mp3' },
-      { id: '3', name: 'Convention Center', code: 'CC003', audioFile: 'route205_stop3.mp3' },
-      { id: '4', name: 'Metro Station', code: 'MS004', audioFile: 'route205_stop4.mp3' }
+      { id: '3', name: 'Convention Center', code: 'CC003', audioFile: 'route101_stop1.mp3' },
+      { id: '4', name: 'Metro Station', code: 'MS004', audioFile: 'route101_stop2.mp3' }
     ]
   },
   {
@@ -31,9 +31,9 @@ export const busRoutes: BusRoute[] = [
     stops: [
       { id: '1', name: 'Residential Area A', code: 'RA001', audioFile: 'route150_stop1.mp3' },
       { id: '2', name: 'Park & Ride', code: 'PR002', audioFile: 'route150_stop2.mp3' },
-      { id: '3', name: 'Shopping Center', code: 'SC003', audioFile: 'route150_stop3.mp3' },
-      { id: '4', name: 'Medical Center', code: 'MC004', audioFile: 'route150_stop4.mp3' },
-      { id: '5', name: 'School Zone', code: 'SZ005', audioFile: 'route150_stop5.mp3' }
+      { id: '3', name: 'Shopping Center', code: 'SC003', audioFile: 'route101_stop1.mp3' },
+      { id: '4', name: 'Medical Center', code: 'MC004', audioFile: 'route101_stop2.mp3' },
+      { id: '5', name: 'School Zone', code: 'SZ005', audioFile: 'route101_stop1.mp3' }
     ]
   },
   {
@@ -43,53 +43,9 @@ export const busRoutes: BusRoute[] = [
     stops: [
       { id: '1', name: 'Beach Resort', code: 'BR001', audioFile: 'route75_stop1.mp3' },
       { id: '2', name: 'Pier Plaza', code: 'PP002', audioFile: 'route75_stop2.mp3' },
-      { id: '3', name: 'Boardwalk', code: 'BW003', audioFile: 'route75_stop3.mp3' },
-      { id: '4', name: 'Marina', code: 'MA004', audioFile: 'route75_stop4.mp3' },
-      { id: '5', name: 'Lighthouse Point', code: 'LP005', audioFile: 'route75_stop5.mp3' }
-    ]
-  },{
-    id: '6',
-    routeNumber: '75',
-    routeName: 'Beach Line',
-    stops: [
-      { id: '1', name: 'Beach Resort', code: 'BR001', audioFile: 'route75_stop1.mp3' },
-      { id: '2', name: 'Pier Plaza', code: 'PP002', audioFile: 'route75_stop2.mp3' },
-      { id: '3', name: 'Boardwalk', code: 'BW003', audioFile: 'route75_stop3.mp3' },
-      { id: '4', name: 'Marina', code: 'MA004', audioFile: 'route75_stop4.mp3' },
-      { id: '5', name: 'Lighthouse Point', code: 'LP005', audioFile: 'route75_stop5.mp3' }
-    ]
-  },{
-    id: '7',
-    routeNumber: '75',
-    routeName: 'Beach Line',
-    stops: [
-      { id: '1', name: 'Beach Resort', code: 'BR001', audioFile: 'route75_stop1.mp3' },
-      { id: '2', name: 'Pier Plaza', code: 'PP002', audioFile: 'route75_stop2.mp3' },
-      { id: '3', name: 'Boardwalk', code: 'BW003', audioFile: 'route75_stop3.mp3' },
-      { id: '4', name: 'Marina', code: 'MA004', audioFile: 'route75_stop4.mp3' },
-      { id: '5', name: 'Lighthouse Point', code: 'LP005', audioFile: 'route75_stop5.mp3' }
-    ]
-  },{
-    id: '8',
-    routeNumber: '75',
-    routeName: 'Beach Line',
-    stops: [
-      { id: '1', name: 'Beach Resort', code: 'BR001', audioFile: 'route75_stop1.mp3' },
-      { id: '2', name: 'Pier Plaza', code: 'PP002', audioFile: 'route75_stop2.mp3' },
-      { id: '3', name: 'Boardwalk', code: 'BW003', audioFile: 'route75_stop3.mp3' },
-      { id: '4', name: 'Marina', code: 'MA004', audioFile: 'route75_stop4.mp3' },
-      { id: '5', name: 'Lighthouse Point', code: 'LP005', audioFile: 'route75_stop5.mp3' }
-    ]
-  },{
-    id: '9',
-    routeNumber: '75',
-    routeName: 'Beach Line',
-    stops: [
-      { id: '1', name: 'Beach Resort', code: 'BR001', audioFile: 'route75_stop1.mp3' },
-      { id: '2', name: 'Pier Plaza', code: 'PP002', audioFile: 'route75_stop2.mp3' },
-      { id: '3', name: 'Boardwalk', code: 'BW003', audioFile: 'route75_stop3.mp3' },
-      { id: '4', name: 'Marina', code: 'MA004', audioFile: 'route75_stop4.mp3' },
-      { id: '5', name: 'Lighthouse Point', code: 'LP005', audioFile: 'route75_stop5.mp3' }
+      { id: '3', name: 'Boardwalk', code: 'BW003', audioFile: 'route101_stop1.mp3' },
+      { id: '4', name: 'Marina', code: 'MA004', audioFile: 'route101_stop2.mp3' },
+      { id: '5', name: 'Lighthouse Point', code: 'LP005', audioFile: 'route101_stop1.mp3' }
     ]
   }
 ];
